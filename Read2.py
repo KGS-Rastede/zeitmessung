dbece09bec90d9aea701788bc49daf502e1bafdb
Wiebke Dirksen <wiebke2.dirksen@ewetel.net>
2016-03-25 18:35:10 +0100

    25/3(dZeit fehlt, mehr Kommentare)

--- Read2.py
+++ Read2.py
@@ -8,7 +8,6 @@ from time import *
 import csv
 import sys
 
-'''ordnet den jeweiligen Chip-UIDs Namen zu'''
 Chip1 = [100, 100,187, 150, 45]
 Chip2 = [211, 153, 104,0, 34]
 Chip3 = [240,175,82,17,28]
@@ -17,13 +16,22 @@ Chip5 = [51,214,8,3,238]
 Chip6 = [132,179,250,110,163]
 Chip7 = [148,126,42,111,175]
 Chip8 = [148,75,248,110,73]
+'''
+ordnet den jeweiligen Chip-UIDs Namen zu
+'''
 
 weiterlesen = True
 
 def aufhoeren(signal, frame):
+'''
+Diese Funktion wird aufgerufen, wenn das Programm mit Strg + C beendet wird
+'''
     global weiterlesen
-    print 'Programm beendet'
     d = open ('Cooper-Test.csv','w')
+    '''
+    Erstellt, bzw. wenn diese Datei bereits vorhanden ist,
+    überschreibt eine CSV-Datei mit dem Namen Cooper-Test
+    '''
     d.write (str('Chipname') + ',' + str('Rundenanzahl') + '\n' + '\n' + 
                  L1[0] + ',' + str(len(L1[1:-1])) + str(' Runden') + '\n' +
                  L2[0] + ',' + str(len(L2[1:-1])) + str(' Runden') + '\n' +
@@ -33,6 +41,11 @@ def aufhoeren(signal, frame):
                  L6[0] + ',' + str(len(L6[1:-1])) + str(' Runden') + '\n' +
                  L7[0] + ',' + str(len(L7[1:-1])) + str(' Runden') + '\n' +
                  L8[0] + ',' + str(len(L8[1:-1])) + str(' Runden'))
+    '''
+    In die CSV-Datei wird in die erste Spalte jeweils der Chipname geschrieben.
+    In der zweiten Spalte steht nun die Anzahl der Runden
+    '''
+    print 'Programm beendet'
     weiterlesen = False
     GPIO.cleanup()
 
@@ -44,47 +57,58 @@ L5 = ['Chip5']
 L6 = ['Chip6']
 L7 = ['Chip7']
 L8 = ['Chip8']
-
-def csv_datei(Chip):
-    f = open('Cooper-Test.csv','w')
-    Titel = ['Chipname''Zeit']
-    f.write(Titel[0] + ';' + Titel[1] + '\n')
+'''
+Acht verschiedene Listen mit einer String als Inhalt werden erzeugt. 
+'''
 
 def Liste_erweitern(x):
     x.append(strftime('%H:%M:%S', localtime()))
+    '''
+    Diese Funktion fügt zu der für das Argument x eingegebenen Liste ein neues Element,
+    welches die aktuelle Zeit im hh:mm:ss Format ausgibt zu. 
+    '''
     print x[0], ':' , len(x[1:-1]), 'Runden'
+    '''
+    Auf dem Bildschirm wird der Chipname, welcher dem ersten Element
+    der für das Argument x eingegebenen Liste entspricht,  ein Doppelpunkt
+    sowie die Anzahl der Runden angezeigt. Der Befehl len(x[1:-1]) zählt die Anzahl der Elemente
+    vom zweiten Element  bis zum letzten Element.
+    '''
 ##    Runde1 = x[1]
 ##    letzteRunde = x[-1])
 ##    diff_sek = letzteRunde - Runde1
 ##    print diff_sek
     sleep(1)
-
+    '''
+    Dieser Befehl pausiert das Lesegerät für eine Sekunde, sodass ein Chip nicht aus Versehen
+    zweimal hintereinander gelesen wird. Somit wird das Ergebnis nicht verfälscht.
+    '''
 
 signal.signal(signal.SIGINT, aufhoeren)
 
 MIFAREReader = MFRC522.MFRC522()
 
-"""
-Nachricht beim Starten des Programmes
-"""
 print ('Herzlich Willkommen!' '\n'
          'Zum Beenden Strg + C druecken')
 
+"""
+Nachricht beim Starten des Programmes erscheint auf dem Bildschirm
+"""
 while weiterlesen:
+    (Status,Kartentyp) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
     """
     nach Karten scannen
     """
-    (Status,Kartentyp) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
-
+    (Status, uid) = MIFAREReader.MFRC522_Anticoll()
     """
     UID bekommen
     """
-    (Status, uid) = MIFAREReader.MFRC522_Anticoll()
-
-    '''Der Chipname wird als Bestätigung ausgegeben'''
+    
     if Status == MIFAREReader.MI_OK:
+    '''
+    Der Chipname wird als Bestätigung ausgegeben
+    '''
         if uid == Chip1:
-
             Liste_erweitern(L1)
         elif uid == Chip2:
             Liste_erweitern(L2)
@@ -102,5 +126,12 @@ while weiterlesen:
             Liste_erweitern(L8)
         else:
             print 'Falscher Chip'
+        '''
+        -   Wenn die vom Chip an das Lesegerät gesendete UID der UID von einem der Chips entspicht,
+            wird die Funktion Liste_erweitern mit dem Argument der dem Chipnamen entsprechenden
+            Liste aufgerufen.  
+        -   Wenn die vom Chip an das Lesegerät gesendete UID nicht der UID von einem der Chips entspicht,
+            erscheint die Nachricht: "Falscher Chip" auf dem Bildschirm.
+        '''
 
         
