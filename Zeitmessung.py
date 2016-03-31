076736725cf128bdcc94fe737e8e0dded0700c92
Wiebke Dirksen <wiebke2.dirksen@ewetel.net>
2016-03-31 16:47:01 +0200

    31/3

--- Zeitmessung.py
+++ Zeitmessung.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+"""Erfasse und speichere Transponder und Zeit in einer CSV-Datei.
 
+
+"""
 import RPi.GPIO as GPIO 
 import MFRC522
 import signal
 from time import *
 import csv
 
-'''ordnet den jeweiligen Chip-UIDs Namen zu'''
+
 Chip1 = [100, 100,187, 150, 45]
 Chip2 = [211, 153, 104,0, 34]
 Chip3 = [240,175,82,17,28]
@@ -20,12 +23,14 @@ Chip8 = [148,75,248,110,73]
 weiterlesen = True
 
 def aufhoeren(signal, frame):
+    """Beende das Programm, und """
     global weiterlesen
     print 'Programm beendet'
     weiterlesen = False
     GPIO.cleanup()
 
 def neue_CSV():
+    """öffne die CSV-Datei Zeitmessung, leere ihren Inhalt und schreibe die Spaltenüberschriften."""
     d = open ('Zeitmessung.csv', 'w+')
     Titel = ['Chipname','Zeit']
     d.write(str('Chipname') + ',' + str('Zeit') + '\n' + '\n')
@@ -34,19 +39,26 @@ def neue_CSV():
 neue_CSV()
 
 def Zeit_einfuegen(Chip):
+    """Füge den Chipnamen und die aktuelle Zeit zu der Datei Zeitmessung.csv hinzu."""
     d = open ('Zeitmessung.csv','a')
     d.write (Chip + ',' + str(strftime('%H:%M:%S', localtime())) + '\n')
     d.close()
     print Chip
     sleep(1)
 
+def LED(x):
+    GPIO.setmode (GPIO.BOARD)
+    GPIO.setup(x, GPIO.OUT)
+    GPIO.output(x, GPIO.HIGH)
+
+    sleep(0.5)
+    GPIO.output(x,GPIO.LOW)
+
 signal.signal(signal.SIGINT, aufhoeren)
 
 MIFAREReader = MFRC522.MFRC522()
 
-"""
-Nachricht beim Starten des Programmes
-"""
+
 print ('Herzlich Willkommen!' '\n'
          'Zum Beenden Strg + C druecken')
 
@@ -61,25 +73,33 @@ while weiterlesen:
     """
     (Status, uid) = MIFAREReader.MFRC522_Anticoll()
 
-    '''Der Chipname wird als Bestätigung ausgegeben'''
     if Status == MIFAREReader.MI_OK:
         if uid == Chip1:
+            LED(7)
             Zeit_einfuegen('Chip1')
         elif uid == Chip2:
+            LED(7)
             Zeit_einfuegen('Chip2')
         elif uid == Chip3:
+            LED(7)
             Zeit_einfuegen('Chip3')
         elif uid == Chip4:
+            LED(7)
             Zeit_einfuegen('Chip4')
         elif uid == Chip5:
+            LED(7)
             Zeit_einfuegen('Chip5')
         elif uid == Chip6:
+            LED(7)
             Zeit_einfuegen('Chip6')
         elif uid == Chip7:
+            LED(7)
             Zeit_einfuegen('Chip7')
         elif uid == Chip8:
+            LED(7)
             Zeit_einfuegen('Chip8')
         else:
+            LED(11)
             print 'Falscher Chip'
 
         
